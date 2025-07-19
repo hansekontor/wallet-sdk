ca7072d7c4a2bbc4eff44e0096f2d3b55a35e86e
Olav <hansekontor@pm.me>
2025-07-19 12:41:54 +0200

    add localforage, vite-plugin-dts and node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
       include: ['lib'],
       tsconfigPath: './tsconfig.lib.json'
     }),
+    nodePolyfills(),
   ],
   build: {
     lib: {
