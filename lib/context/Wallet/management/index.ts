9fff28c90d759180c4fd907c5b3ca3f6ff907296
Olav <hansekontor@pm.me>
2025-07-24 11:05:19 +0200

    remove unused components

--- lib/context/Wallet/management/index.ts
+++ lib/context/Wallet/management/index.ts
@@ -1,45 +1,5 @@
 import { type Wallet } from '../types';
 
-export const activateWallet = () => {
-
-}
-
-export const activateNewWallet = () => {
-
-}
-
-export const addNewSavedWallet = () => {
-
-}
-
-export const deleteWallet = () => {
-
-}
-
-export const getActiveWallet = () => {
-
-}
-
-export const getSavedWallet = () => {
-
-}
-
-export const getWallet = () => {
-
-}
-
-export const getWalletDetails = () => {
-
-}
-
-export const getWalletOnStartup = () => {
-
-}
-
-export const renameWallet = () => {
-
-}
-
 export const getWalletOrder = (activeWallet: Wallet, wallets: Wallet[]) => {
     const walletOrder = [activeWallet];
     for (let i = 0; i < wallets.length; i++) {
