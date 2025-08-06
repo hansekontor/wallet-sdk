64e450ffe48d63b622d0a97cc43b95bf0ca70ea3
Olav <hansekontor@pm.me>
2025-08-06 17:55:12 +0200

    remove unused export

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,6 @@ export * from './providers/MUSDProvider';
 // contexts
 export * from "./context/Auth";
 export * from "./context/App";
-export * from "./context/Wallet";
 
 // utils
 export * from "./utils/events";
